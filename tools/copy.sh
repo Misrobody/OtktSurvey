ad3b43765bf2f628f7a00a71b8b7d48b45bc061a
larrivain <daphne.larrivain@ecole.ensicaen.fr>
2025-05-22 10:31:22 +0200

    fix

---
+++ tools/copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Copy the built otkt distrib from the sub-repository to the tools directory
+
+cp OtktDSL/kieker.otel.translation.parent/kieker.otel.translation.generator/target/otkt-jar-with-dependencies.jar ./tools/
